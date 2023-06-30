0a5b04524e48ca3b4be0b3997c8391297ab68620
Artur <ivan_ivankov@mail.ru>
2023-06-30 12:47:44 +0300

    Задача 1

---
+++ Study_1/1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшое цифру числа.
+
+// Random rnd = new Random();
+// int num = new rnd.Next(10, 100);
+// 54 / 10 = 5
+// 54 % 10 = 4
+
+int RandomNum ()
+{
+    int num = new Random().Next(10, 100);
+    Console.WriteLine($"Число {num}");
+    return num;
+}
+
+int MaxDigit (int number)
+{
+    int first = number / 10;
+    int second = number % 10;
+    if (first > second)
+        return first;
+    else
+        return second;
+}
+
+int rand = RandomNum();
+int maxDigit = MaxDigit(rand);
+Console.WriteLine($"Макс цифра в числе {rand} = {maxDigit}");
