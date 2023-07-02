809c73b65c914c1b28b12b6840647bd440dae8ce
Artur <ivan_ivankov@mail.ru>
2023-07-02 12:34:40 +0300

    3 задача

---
+++ Study_1/3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+int FindResult(int number1, int number2)
+{
+    result = number1 % number2;
+    return result;
+}
+
+int res = FindResult(number1, number2);
+if (res == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Некратно, остаток {res}");
+} 
