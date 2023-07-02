7d87d6f0eeaae5e2839759dfe7fb65f7fec77c1c
Artur <ivan_ivankov@mail.ru>
2023-07-02 12:41:19 +0300

    3 задача 2 вариант

---
+++ Study_1/3_Option_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// 2 Вариант решения 
+
+int Numbers (string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int number1 = Numbers("Введите первое число");
+int number2 = Numbers("Введите второе число");
+
+void Division(int num1, int num2)
+{
+    int two = num1 % num2;
+    if (two == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Некратно, " + two);
+    }
+}
+
+Division (number1, number2);
