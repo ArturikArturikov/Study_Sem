61a559d266a0e3d18890c4b0dd5a93bf3d799569
Artur <ivan_ivankov@mail.ru>
2023-07-02 13:41:32 +0300

    Вернуть два числа

---
+++ Study_3/1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вернуть два числа.
+
+int[] InputSomeNums()
+{
+    int[] twoNums = new int[2];
+    Console.Write("Введите первое число: ");
+    twoNums[0] = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите второе число: ");
+    twoNums[1] = int.Parse(Console.ReadLine()!);
+    return twoNums;
+}
+
+int[] array = InputSomeNums();
+Console.WriteLine($"{array[0]}, {array[1]}");
