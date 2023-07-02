7086edafc0583105d97127956817a298e3e19eaa
Artur <ivan_ivankov@mail.ru>
2023-07-02 13:04:43 +0300

    На вход принимает число и выдает его квадрат

---
+++ Study_2/1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
+
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} равен {square}");
