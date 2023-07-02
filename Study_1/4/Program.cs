8d36654b7afa290c8cd62c82a55dd30f5c307663
Artur <ivan_ivankov@mail.ru>
2023-07-02 12:44:59 +0300

    Задача 4

---
+++ Study_1/4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно с 7 и 23.
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+string Multiplicity(int number)
+{
+    string res = "Нет";
+    if (number % 7 == 0 && number % 23 == 0)
+        res = "Да";
+    return res;    
+}
+
+int numbers = InputNum("Введите целое число: ");
+string result = Multiplicity(numbers);
+
+Console.WriteLine(result);
