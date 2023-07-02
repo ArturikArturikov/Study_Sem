6bc12cb69770df3a863fe4b7390b0e168f05d4ae
Artur <ivan_ivankov@mail.ru>
2023-07-02 13:45:37 +0300

    Ставим точку в конце строки

---
+++ Study_3/2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Ставим точку в конце строки
+
+int[] InputSomeNums()
+{
+    Console.Write("Введите количество чисел: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-e число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+// int[] array = InputSomeNums();
+// Console.WriteLine($"{array[0]}, {array[1]}");
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+        else
+            Console.Write("."); 
+    }
+}
+
+int[] maArray = InputSomeNums();
+PrintArray(maArray);
