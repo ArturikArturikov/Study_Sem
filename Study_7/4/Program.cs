7f7a98c8abe5ded2062466bc992862e2cbd9be19
Artur <ivan_ivankov@mail.ru>
2023-07-03 12:24:43 +0300

    преобразовывать десятичное число в двоичное

---
+++ Study_7/4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу,
+// которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string Bin(int num)
+{
+    string num2 = "";
+    while (num > 0)
+    {
+        int digit = num % 2;
+        num2 = $"{digit}" + num2;
+        num = num / 2;
+    }
+    return num2;
+}
+
+int num3 = InputNum("Введите число: ");
+Console.WriteLine(Bin(num3));
