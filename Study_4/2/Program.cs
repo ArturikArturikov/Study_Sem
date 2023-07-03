7c3e04aea9c37a4d9d57c4120a649d86f3634a31
Artur <ivan_ivankov@mail.ru>
2023-07-03 10:23:12 +0300

    Перевернуть число и сравнить его (палидром)

---
+++ Study_4/2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Код перевернуть число и сравнить его (палидром).
+
+long InputNum(string message)
+{
+    Console.Write(message);
+    return long.Parse(Console.ReadLine()!);
+}
+
+long ReverseNum(long number)
+{
+    long reversedNum = 0;
+    while (number != 0)
+    {
+        reversedNum = reversedNum * 10 + number % 10;
+        number /= 10;
+    }
+    return reversedNum;
+}
+
+bool Palindrome(long num, long rev)
+{
+    return num == rev;
+}
+
+long num = InputNum("Введите число: ");
+long rev = ReverseNum(num);
+bool res = Palindrome(num, rev);
+Console.WriteLine(res);
