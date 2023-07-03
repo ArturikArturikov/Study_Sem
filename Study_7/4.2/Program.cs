59dcc7511699b4ddbfe7e7422974763b8e6eb753
Artur <ivan_ivankov@mail.ru>
2023-07-03 12:29:17 +0300

    вариант 3

---
+++ Study_7/4.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.WriteLine("число: ");
+int num = int.Parse(Console.ReadLine());
+string count = "";
+
+while (num > 0)
+{
+	count = num % 2 + count;
+	num = num / 2;
+}
+System.Console.WriteLine(count);
