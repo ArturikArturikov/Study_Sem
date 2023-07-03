969f848a9f60fb8eefde961b74be5fb8cfb1f63f
Artur <ivan_ivankov@mail.ru>
2023-07-03 12:21:01 +0300

    принимает три числа и проверяет может существовать треугольник

---
+++ Study_7/3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длины.
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Triangle(int a, int b, int c)
+{
+    if (a + b < c || b + c < a || a + c < b)
+    Console.WriteLine("Нет");
+    else 
+    Console.WriteLine("Да");
+}
+
+int num = InputNum("Введите первое число: ");
+int num1 = InputNum("Введите второе число: ");
+int num2 = InputNum("Введите третье число: ");
+
+Triangle(num, num1, num2);
